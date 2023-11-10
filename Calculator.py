01ef89f3e5563a6a0662725f4080dd79d6625d00
MrWumpi7000 <jasper@grevsmuehl.net>
2023-11-10 16:56:53 -0500

    updated light / dark mode

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,11 @@
 import customtkinter
 import tkinter as tk
+import random 
 from tkinter import filedialog
 
+mode = "dark"
 
-customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_appearance_mode(mode)  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
@@ -62,7 +64,7 @@ entry = customtkinter.CTkEntry(
     master=app,
     placeholder_text="Rechnung eingeben:",
     height=50,
-    text_color='white',
+    text_color='grey',
     placeholder_text_color='#b8b4b4',
     corner_radius=17,
     font= ("",17)
@@ -91,6 +93,45 @@ button = customtkinter.CTkButton(
     )
 button.pack(fill='both')
 
+def openNewWindow():
+     
+    newWindow = customtkinter.CTkToplevel()
+    newWindow.title("Random Generator")
+    newWindow.geometry("400x400")
+    newWindow._set_appearance_mode(mode)
+
+def switch_event():
+    if switch_var.get() == 'on':
+        customtkinter.set_appearance_mode('dark') 
+        mode = 'dark'
+    if switch_var.get() == 'off':
+        customtkinter.set_appearance_mode('light') 
+        mode = 'light'
+        
+randombutton = customtkinter.CTkButton(
+    master=app, 
+    text='Random Generator', 
+    command=openNewWindow,
+    height=50,
+    fg_color="#4a4949",
+    corner_radius=17,
+    font= ("",17))
+randombutton.pack(fill='both')
+
+switch_var = customtkinter.StringVar(value="on")
+switch = customtkinter.CTkSwitch(
+    app,
+    text="Dark mode",
+    command=switch_event,
+    variable=switch_var, 
+    onvalue="on", 
+    offvalue="off",
+    fg_color="#4a4949",
+    corner_radius=17,
+    font= ("",17)
+)
+switch.pack(side='bottom')
+
 buttonclose = customtkinter.CTkButton(
     master=app, 
     text='Beenden...', 
@@ -100,6 +141,6 @@ buttonclose = customtkinter.CTkButton(
     corner_radius=17,
     font= ("",17)
     )
-buttonclose.pack(fill='both')
+buttonclose.pack(fill='both', side='bottom')
 
 app.mainloop()
