d646ea26becaf3f41125fe4e3cf6353dcc4bd8bc
MrWumpi7000 <jasper@grevsmuehl.net>
2023-11-12 17:36:25 -0500

    added random generator

--- Calculator.py
+++ Calculator.py
@@ -9,10 +9,11 @@ customtkinter.set_appearance_mode(mode)  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("400x240")
+app.geometry("700x400")
 app.title('Calculator')
 
 label =""
+labelslider = ''
 
 def stop():
     app.quit()
@@ -92,13 +93,173 @@ button = customtkinter.CTkButton(
     font= ("",17)
     )
 button.pack(fill='both')
+def getRandomZeichen(SWITCHPLUS, SWITCHMINUS, SWITCHGETEILT, SWITCHMAL):
+    listezeichen = []
+
+    if SWITCHPLUS == 'on':
+        listezeichen.append('+')
+    if SWITCHMINUS == 'on':
+        listezeichen.append('-')
+    if SWITCHGETEILT == 'on':
+        listezeichen.append('/')
+    if SWITCHMAL == 'on':
+        listezeichen.append('*')
+#    if listezeichen.count > 0:
+    return random.choice(listezeichen)
+#    else:
+    print('Geht nicht......')
+     
+
+def berechnungrandom(entry1, entry2, scroolbar, SWITCHPLUS, SWITCHMINUS, SWITCHMAL, SWITCHGETEILT):
+    Value1 = (int(entry1))
+    Value2 = (int(entry2))
+    SCROOLBAR = (int(scroolbar))
+    
+    print(SCROOLBAR)
+    for i in range(1, SCROOLBAR):
+        ZAHLEN = random.randint(Value1, Value2)
+        Ergenis = ZAHLEN, getRandomZeichen(SWITCHPLUS,SWITCHMINUS,SWITCHGETEILT,SWITCHMAL)
 
 def openNewWindow():
-     
+    global entry1
+    global entry2
     newWindow = customtkinter.CTkToplevel()
     newWindow.title("Random Generator")
-    newWindow.geometry("400x400")
+    newWindow.geometry("800x400")
     newWindow._set_appearance_mode(mode)
+
+    def button_event():
+        global entry1
+        global entry2
+
+        slidervalue = slider.get()
+        switchplus = switch_var1.get()
+        switchminus = switch_var2.get()
+        switchmal = switch_var3.get()
+        switchgeteilt = switch_var4.get()
+        zahl1 = entry1.get()
+        zahl2 = entry2.get()
+        berechnungrandom(zahl1, zahl2, slidervalue, switchplus, switchminus, switchmal, switchgeteilt)
+        newWindow.destroy()
+    Prozeedbutton = customtkinter.CTkButton(
+        newWindow, 
+        text="Aufgabe Generieren!", 
+        command=button_event,
+        height=50,
+        fg_color="#4a4949",
+        corner_radius=17,
+        font= ("",17)
+    )
+    
+    Prozeedbutton.pack(fill='both',side='bottom')
+    entry1= customtkinter.CTkEntry(
+    newWindow, 
+    placeholder_text="Start vom benutzten Zahlenraum",
+    height=50,
+    text_color='grey',
+    placeholder_text_color='#b8b4b4',
+    corner_radius=17,
+    font= ("",17)
+        )
+    entry1.pack(fill='both')
+    entry2= customtkinter.CTkEntry(
+    newWindow,
+    placeholder_text="Ende vom benutzten Zahlenraum",
+    height=50,
+    text_color='grey',
+    placeholder_text_color='#b8b4b4',
+    corner_radius=17,
+    font= ("",17)
+        )
+    entry2.pack(fill='both')
+    def slider_event(value):
+        global labelslider
+        try:
+            labelslider.destroy()
+        except:
+            print('nichts zum löschen')
+        labelslider = customtkinter.CTkLabel(
+            newWindow, 
+            text=f"Anzahl der Benutzen Zahlen: {(int(value))}",
+            height=50,
+            corner_radius=17,
+            text_color='white',
+            fg_color='#787575',
+            font= ("",17)
+        )
+        labelslider.pack(fill='both')
+        print(value)
+
+    slider = customtkinter.CTkSlider(
+        newWindow, 
+        from_=1, 
+        to=10, 
+        command=slider_event,
+        number_of_steps=9,
+        height=20,
+        fg_color="#4a4949"
+    )
+    slider.pack(fill='both')
+
+    def switch_event():
+        print("switch toggled, current value:", switch_var1.get())
+        print(switch_var2.get())
+        print(switch_var3.get())
+        print(switch_var4.get())
+
+    switch_var1 = customtkinter.StringVar(value="off")
+    switch1 = customtkinter.CTkSwitch(
+        newWindow, 
+        text="+", 
+        command=switch_event,
+        variable=switch_var1, 
+        onvalue="on", 
+        offvalue="off",
+        switch_height=30,
+        switch_width=60,
+        fg_color="#4a4949"
+        )
+    switch1.pack(side='right')
+    switch_var2 = customtkinter.StringVar(value="off")
+    switch2 = customtkinter.CTkSwitch(
+        newWindow, 
+        text="-", 
+        command=switch_event,
+        variable=switch_var2, 
+        onvalue="on", 
+        offvalue="off",
+        switch_height=30,
+        switch_width=60,
+        fg_color="#4a4949"
+        )
+    switch2.pack(side='right')
+    switch_var3 = customtkinter.StringVar(value="off")
+    switch3 = customtkinter.CTkSwitch(
+        newWindow, 
+        text="*", 
+        command=switch_event,
+        variable=switch_var3, 
+        onvalue="on", 
+        offvalue="off",
+        switch_height=30,
+        switch_width=60,
+        fg_color="#4a4949"
+        )
+    switch3.pack(side='right')
+    switch_var4 = customtkinter.StringVar(value="off")
+    switch4 = customtkinter.CTkSwitch(
+        newWindow, 
+        text=":", 
+        command=switch_event,
+        variable=switch_var4, 
+        onvalue="on", 
+        offvalue="off",
+        switch_height=30,
+        switch_width=60,
+        fg_color="#4a4949"
+        )
+    switch4.pack(side='right')
+
 
 def switch_event():
     if switch_var.get() == 'on':
@@ -143,4 +304,6 @@ buttonclose = customtkinter.CTkButton(
     )
 buttonclose.pack(fill='both', side='bottom')
 
+def segmented_button_callback(value):
+    print("segmented button clicked:", value)
 app.mainloop()
